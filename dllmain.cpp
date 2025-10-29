7b96633d5162c34d60a939da4aa35a01041a3fdd
revo-xx <yd9240367@gmail.com>
2025-10-30 01:37:47 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -4,11 +4,58 @@
 
 FILE *pFile = nullptr;
 
-void init(HMODULE hModule)
+JavaVM *vm;
+JNIEnv *env = NULL;
+
+void Init(HMODULE hModule)
 {
     AllocConsole();
     freopen_s(&pFile, "CONOUT$", "w", stdout);
     printf("DLL Injected Successfully!\n");
+
+    jsize vmCount;
+    if (JNI_GetCreatedJavaVMs(&vm, 1, &vmCount) != JNI_OK || vmCount == 0)
+    {
+        printf("Failed to get Java VM\n");
+        return;
+    }
+
+    jint res = vm->GetEnv((void **)&env, JNI_VERSION_1_6);
+    if (res == JNI_EDETACHED)
+    {
+        res = vm->AttachCurrentThread((void **)&env, nullptr);
+    }
+    if (res != JNI_OK)
+    {
+        printf("Failed to attach current thread to Java VM\n");
+        return;
+    }
+    if (env != nullptr)
+    {
+        printf("Successfully obtained JNIEnv pointer\n");
+
+        jclass minecraftClass = env->FindClass("ave");
+        jmethodID getMinecraft = env->GetStaticMethodID(minecraftClass, "A", "()Lave;");
+        jobject minecraftInstance = env->CallStaticObjectMethod(minecraftClass, getMinecraft);
+
+        jfieldID playerField = env->GetFieldID(minecraftClass, "h", "Lbew;");
+        jobject player = env->GetObjectField(minecraftInstance, playerField);
+
+        if (player == NULL)
+        {
+            printf("Player object is NULL\n");
+            return;
+        }
+
+        jclass playerClass = env->GetObjectClass(player);
+
+        jmethodID jumpMethod = env->GetMethodID(playerClass, "bF", "()V");
+        env->CallVoidMethod(player, jumpMethod);
+    }
+    else
+    {
+        printf("Failed to obtain JNIEnv pointer\n");
+    }
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule,
@@ -18,7 +65,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)init, hModule, 0, 0);
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Init, hModule, 0, 0);
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
