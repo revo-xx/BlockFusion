39f689253bf40da8a62e217618fa59f831dc0787
revo-xx <yd9240367@gmail.com>
2025-10-30 02:21:05 +0300

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -4,70 +4,74 @@
 
 FILE *pFile = nullptr;
 
-JavaVM *vm;
-JNIEnv *env = NULL;
+JavaVM *vm = nullptr;
+JNIEnv *env = nullptr;
 
 void Init(HMODULE hModule)
 {
     AllocConsole();
     freopen_s(&pFile, "CONOUT$", "w", stdout);
-    printf("DLL Injected Successfully!\n");
+    printf("[+] DLL Injected Successfully!\n");
 
     jsize vmCount;
     if (JNI_GetCreatedJavaVMs(&vm, 1, &vmCount) != JNI_OK || vmCount == 0)
     {
-        printf("Failed to get Java VM\n");
-        return;
+        printf("[-] Failed to get Java VM\n");
+        goto cleanup;
     }
 
     jint res = vm->GetEnv((void **)&env, JNI_VERSION_1_6);
     if (res == JNI_EDETACHED)
-    {
         res = vm->AttachCurrentThread((void **)&env, nullptr);
-    }
-    if (res != JNI_OK)
+
+    if (res != JNI_OK || env == nullptr)
     {
-        printf("Failed to attach current thread to Java VM\n");
-        return;
+        printf("[-] Failed to attach current thread to Java VM\n");
+        goto cleanup;
     }
-    if (env != nullptr)
+
+    printf("[+] Successfully obtained JNIEnv pointer\n");
+
+    jclass minecraftClass = env->FindClass("ave");
+    if (!minecraftClass)
     {
-        printf("Successfully obtained JNIEnv pointer\n");
-
-        jclass minecraftClass = env->FindClass("ave");
-        jmethodID getMinecraft = env->GetStaticMethodID(minecraftClass, "A", "()Lave;");
-        jobject minecraftInstance = env->CallStaticObjectMethod(minecraftClass, getMinecraft);
-
-        jfieldID playerField = env->GetFieldID(minecraftClass, "h", "Lbew;");
-        jobject player = env->GetObjectField(minecraftInstance, playerField);
-
-        if (player == NULL)
-        {
-            printf("Player object is NULL\n");
-            return;
-        }
-
-        jclass playerClass = env->GetObjectClass(player);
-
-        jmethodID jumpMethod = env->GetMethodID(playerClass, "bF", "()V");
-        env->CallVoidMethod(player, jumpMethod);
+        printf("[-] Failed to find Minecraft class\n");
+        goto cleanup;
     }
-    else
+
+    jmethodID getMinecraft = env->GetStaticMethodID(minecraftClass, "A", "()Lave;");
+    jobject minecraftInstance = env->CallStaticObjectMethod(minecraftClass, getMinecraft);
+
+    jfieldID playerField = env->GetFieldID(minecraftClass, "h", "Lbew;");
+    jobject player = env->GetObjectField(minecraftInstance, playerField);
+
+    if (player == nullptr)
     {
-        printf("Failed to obtain JNIEnv pointer\n");
+        printf("[-] Player object is NULL\n");
+        goto cleanup;
     }
+
+    jclass playerClass = env->GetObjectClass(player);
+    jmethodID jumpMethod = env->GetMethodID(playerClass, "bF", "()V");
+    env->CallVoidMethod(player, jumpMethod);
+
+    printf("[+] Jump method called successfully!\n");
+
+cleanup:
+    Sleep(1000);
+    fclose(pFile);
+    FreeConsole();
+    FreeLibraryAndExitThread(hModule, 0);
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-                      DWORD ul_reason_for_call,
-                      LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
-    switch (ul_reason_for_call)
+    switch (reason)
     {
     case DLL_PROCESS_ATTACH:
-        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Init, hModule, 0, 0);
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
+        DisableThreadLibraryCalls(hModule);
+        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)Init, hModule, 0, nullptr);
+        break;
     case DLL_PROCESS_DETACH:
         break;
     }
